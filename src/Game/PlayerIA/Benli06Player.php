dfb1392b4e62135d9abf71031dfc25673a24784a
Lea Benamozig <lea.benamozig@epita.fr>
2018-12-14 21:49:39 +0100

    change friendChoice

--- src/Game/PlayerIA/Benli06Player.php
+++ src/Game/PlayerIA/Benli06Player.php
@@ -20,17 +20,21 @@ class Benli06Player extends Player
     {
       //$this->prettyDisplay();
 
-      $dream_team = array('PacoTheGreat', 'Felixdupriez', 'Shiinsekai', 'Ghope', 'Etienneelg', 'Christaupher');
+      $dream_team = array('PacoTheGreat', 'Felixdupriez', 'Shiinsekai', 'Ghope', 'Etienneelg', 'Christaupher', 'Galtar95');
       $oppName = $this->result->getStatsFor($this->opponentSide)['name'];
       if (in_array($oppName, $dream_team))
         return parent::friendChoice();
+
+      $statsFriend = $this->result->getStatsFor($this->opponentSide)['friend'];
+      $statsFoe = $this->result->getStatsFor($this->opponentSide)['foe'];
 
       //If point opponnent > point my => FOE
       if($this->result->getLastScoreFor($this->opponentSide) > $this->result->getLastScoreFor($this->mySide))
         return parent::foeChoice();
 
       //If opponent lastChoice is friend => FRIEND
-      if (!$this->result->getLastChoiceFor($this->opponentSide))
+     // if (!$this->result->getLastChoiceFor($this->opponentSide))
+      if (count($statsFriend) > count($statsFoe))
         return parent::friendChoice();
 
       return parent::foeChoice();
